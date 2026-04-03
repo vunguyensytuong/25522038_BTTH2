af3fcc16d048a0093decff017941609c659c0476
vunguyensytuong <vunguyensytuong@gmail.com>
2026-04-03 19:51:03 +0700

    NgayThangNam.h

---
+++ NgayThangNam.h
@@ -0,0 +1,14 @@
+#ifndef NGAYTHANGNAM_H
+#define NGAYTHANGNAM_H
+
+class NgayThangNam
+{
+private:
+    int iNgay, iThang, iNam;
+public:
+    void Nhap();
+    void Xuat();
+    NgayThangNam NgayThangNamTiepTheo();
+};
+
+#endif // NGAYTHANGNAM_H
