78d3f5f9bfc0a8284330d38321529506a9577121
vunguyensytuong <vunguyensytuong@gmail.com>
2026-04-03 20:04:57 +0700

    GioPhutGiay.h

---
+++ GioPhutGiay.h
@@ -0,0 +1,14 @@
+#ifndef GIOPHUTGIAY_H
+#define GIOPHUTGIAY_H
+
+class GioPhutGiay
+{
+private:
+    int iGio,iPhut,iGiay;
+public:
+    void Nhap();
+    void Xuat();
+    GioPhutGiay TinhCongThemMotGiay();
+};
+
+#endif // GIOPHUTGIAY_H
